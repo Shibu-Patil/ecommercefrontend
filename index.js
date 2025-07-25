3b9bce2e5e4407454ebac3e55fcfcbb7001d70c4
Shibu-Patil <patilshibu@gmail.com>
2025-07-25 11:53:24 +0530

    login and register structure created

---
+++ index.js
@@ -0,0 +1,22 @@
+import login from "./login.js"
+import register from "./register.js"
+
+const root=document.getElementById('root')
+const allAnchors=document.querySelectorAll('a')
+
+const router={
+    "/login":login,
+    "/register":register
+}
+
+function handelClick(e){
+  e.preventDefault()
+//   console.log(e.target.pathname);
+let path=e.target.pathname
+  history.pushState(null,"",`${path}`)
+root.innerHTML=router[path]()
+}
+allAnchors.forEach((anchor)=>{
+    anchor.addEventListener("click",handelClick)
+})
+
