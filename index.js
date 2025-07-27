51f0ae0bba1124e4af1b82186283382d4ea85c94
Shibu-Patil <patilshibu@gmail.com>
2025-07-27 11:56:08 +0530

    login page done and functions created for registration page

--- index.js
+++ index.js
@@ -31,3 +31,44 @@ root.innerHTML=""
   
 })
 
+
+
+const state={
+  setState(name,value){
+    this[name]=value
+  }
+}
+const form=document.querySelector('form')
+const inputs=document.querySelectorAll('input')
+const textArea=document.querySelector('textarea')
+
+
+function handelChnage(e){
+let {name,value,files}=e.target
+if(name!="re-password"){
+  if(name=="image"){
+value=files[0]
+const reader=new FileReader()
+reader.onload=function(){
+  form.style.backgroundImage=`url(${reader.result})`
+}
+reader.readAsDataURL(value)
+state.setState(name,value)
+  }else{
+  state.setState(name,value)
+
+  }
+}
+}
+
+function handelSubmit(e){
+e.preventDefault()
+console.log(state);
+
+}
+
+form.addEventListener('submit',handelSubmit)
+inputs.forEach(input=>{
+  input.addEventListener('change',handelChnage)
+})
+textArea.addEventListener('change',handelChnage)
--- login.js
+++ login.js
@@ -1,6 +1,29 @@
 const login=()=>{
     return `
-    <h1>i am login page</h1>
+ 
+<div class="registerForm">
+    <form action="" class="loginForm">
+        <div>
+            <h1>Login</h1>
+        </div>
+       
+        <div>
+            <input type="email" name="email" placeholder="email">
+            <span><i class="fa-solid fa-envelope"></i></span>
+        </div>
+        <div>
+            <input type="password" name="password" placeholder="password">
+            <span><i class="fa-solid fa-key"></i></span>
+        </div>
+
+      
+
+        <div>
+           <button>Submit</button>
+        </div>
+    </form>
+</div>
+
     
     `
 }
