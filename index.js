15852158705cbe06cd341d4913a23da58abde0c1
Shibu-Patil <patilshibu@gmail.com>
2025-07-28 11:50:10 +0530

    cpmpleted register page

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const textArea=document.querySelector('textarea')
 
 function handelChnage(e){
 let {name,value,files}=e.target
-if(name!="re-password"){
+
   if(name=="image"){
 value=files[0]
 const reader=new FileReader()
@@ -58,17 +58,54 @@ state.setState(name,value)
   state.setState(name,value)
 
   }
+
 }
+
+
+function checkPasssword(e){
+ let {name,value}=e.target
+  if(name=="re-password"){
+// console.log(e.target.parentElement);
+
+    state.password!=value?e.target.parentElement.style.borderBottom="3px solid red":e.target.parentElement.style.borderBottom="3px solid black"
+  }else{
+    return
+  }
 }
 
 function handelSubmit(e){
 e.preventDefault()
-console.log(state);
+// console.log(state);
+let {name,email,password,address,image}=state
+if(!name||!email||!password||!address||!image){
+  alert("All Feilds are mandatory")
+  return
+}
+
+console.log(password,state);
+if(password!=state["re-password"]){
+  
+  alert("password and re-passsword should match")
+  return
+}
+// console.log(state);
+
+let payload={email,password,image,address,name}
+console.log(payload);
+
 
 }
+
+
+
+
 
 form.addEventListener('submit',handelSubmit)
 inputs.forEach(input=>{
   input.addEventListener('change',handelChnage)
+})
+
+inputs.forEach(input=>{
+  input.addEventListener('input',checkPasssword)
 })
 textArea.addEventListener('change',handelChnage)
