9a9da352a0079e7fbd1c1f4f3e428c4391360504
Shibu-Patil <patilshibu@gmail.com>
2025-07-29 11:50:01 +0530

    register page completed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import login from "./login.js"
-import register from "./register.js"
+import register, { handelRegisterBind } from "./register.js"
 
 const root=document.getElementById('root')
 const allAnchors=document.querySelectorAll('a')
 
 const router={
-    "/login":login,
-    "/register":register
+    "/login":[login],
+    "/register":[register,handelRegisterBind]
 }
 
 function handelClick(e){
@@ -14,7 +14,10 @@ function handelClick(e){
 //   console.log(e.target.pathname);
 let path=e.target.pathname
   history.pushState(null,"",`${path}`)
-root.innerHTML=router[path]()
+root.innerHTML=router[path][0]()
+if(router[path][1]){
+  router[path][1]()
+}
 }
 allAnchors.forEach((anchor)=>{
     anchor.addEventListener("click",handelClick)
@@ -33,79 +36,3 @@ root.innerHTML=""
 
 
 
-const state={
-  setState(name,value){
-    this[name]=value
-  }
-}
-const form=document.querySelector('form')
-const inputs=document.querySelectorAll('input')
-const textArea=document.querySelector('textarea')
-
-
-function handelChnage(e){
-let {name,value,files}=e.target
-
-  if(name=="image"){
-value=files[0]
-const reader=new FileReader()
-reader.onload=function(){
-  form.style.backgroundImage=`url(${reader.result})`
-}
-reader.readAsDataURL(value)
-state.setState(name,value)
-  }else{
-  state.setState(name,value)
-
-  }
-
-}
-
-
-function checkPasssword(e){
- let {name,value}=e.target
-  if(name=="re-password"){
-// console.log(e.target.parentElement);
-
-    state.password!=value?e.target.parentElement.style.borderBottom="3px solid red":e.target.parentElement.style.borderBottom="3px solid black"
-  }else{
-    return
-  }
-}
-
-function handelSubmit(e){
-e.preventDefault()
-// console.log(state);
-let {name,email,password,address,image}=state
-if(!name||!email||!password||!address||!image){
-  alert("All Feilds are mandatory")
-  return
-}
-
-console.log(password,state);
-if(password!=state["re-password"]){
-  
-  alert("password and re-passsword should match")
-  return
-}
-// console.log(state);
-
-let payload={email,password,image,address,name}
-console.log(payload);
-
-
-}
-
-
-
-
-
-form.addEventListener('submit',handelSubmit)
-inputs.forEach(input=>{
-  input.addEventListener('change',handelChnage)
-})
-
-inputs.forEach(input=>{
-  input.addEventListener('input',checkPasssword)
-})
-textArea.addEventListener('change',handelChnage)
